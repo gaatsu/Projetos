f1b5e686cb442d542c17163b55ace3b8f0b34771
Alan <hitsugaya-br@hotmail.com>
2023-08-21 21:27:12 -0300

    fix: submitJSGetElement

--- Projeto/script.js
+++ Projeto/script.js
@@ -2,4 +2,9 @@ const fname = document.getElementById('fname');
 const lname = document.getElementById('lname');
 const email = document.getElementById('email');
 const cpfnum = document.getElementById('cpfnum');
-const submit = document.getElementById('submit');
+const submit = document.getElementsByClassName('form-register')[0];
+
+submit.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    console.log("Clicado")
+})
