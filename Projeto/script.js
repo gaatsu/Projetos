012cdc6caecc63a76114103a5efb3d198eae2309
Alan <hitsugaya-br@hotmail.com>
2023-08-21 21:24:02 -0300

    feat: startJS+newcssfeat

---
+++ Projeto/script.js
@@ -0,0 +1,5 @@
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const cpfnum = document.getElementById('cpfnum');
+const submit = document.getElementById('submit');
