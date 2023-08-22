63a2ce226a832770845e835004ce7ad1feb953cd
Alan <hitsugaya-br@hotmail.com>
2023-08-21 21:58:28 -0300

    feat: emailsentonclick

--- Projeto/script.js
+++ Projeto/script.js
@@ -7,4 +7,29 @@ const submit = document.getElementsByClassName('form-register')[0];
 submit.addEventListener('submit', (e)=>{
     e.preventDefault();
     console.log("Clicado")
+
+
+    let ebody = `
+    <b>Name:</b>${fname.value}&nbsp;${lname.value}
+    <br>
+    <b>Email:</b>${email.value}
+    <br>
+    
+    `
+
+    Email.send({
+        SecureToken : "4c3e6e1b-8b4e-4072-b5bd-b86a7d78919f",
+        To : (email.value),
+        From : "hitsugaya-br@hotmail.com",
+        Subject : "E-mail de verificação (EMPRESA)",
+        Body : ebody
+    }).then(
+      message => alert(message)
+    );
 })
+
+
+
+//049B428AE1AFDF70EB91D348501271F48F9A
+//smtp.elasticemail.com:2525
+//4c3e6e1b-8b4e-4072-b5bd-b86a7d78919f 
